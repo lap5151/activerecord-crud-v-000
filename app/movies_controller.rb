6c7bdd0918914d9c8345fa94fcac37d4ff7d2d45
Lauren Pardee <lap5151@gmail.com>
2019-02-13 23:24:15 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -53,7 +53,6 @@ def can_find_using_where_clause_and_be_sorted
   # release date descending
   movies = Movie.where(:release_date == 2002)
   binding.pry
-
 end
 
 def can_be_found_updated_and_saved

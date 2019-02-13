a997f5dfa618f0544ffa8c13bc5bef2d8cb089c0
Lauren Pardee <lap5151@gmail.com>
2019-02-13 13:31:14 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -25,7 +25,7 @@ def can_be_created_with_a_hash_of_attributes
   movie
 end
 
-def can_be_created_in_a_block(args = {:title => nil})
+def can_be_created_in_a_block(args = {:title => "Home Alone", :relsease_date => 1990, :director => nil, :lead => nil, :in_theaters => nil})
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990

fccfacfd6d07d4f9df21815670dad0695886f9ee
Lauren Pardee <lap5151@gmail.com>
2019-02-13 23:10:43 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -6,9 +6,9 @@
 # end                              # end
 
 def can_be_instantiated_and_then_saved
-  movie = Movie.new #__
+  movie = Movie.new
   movie.title = "This is a title."
-  movie.save #__
+  movie.save
 end
 
 def can_be_created_with_a_hash_of_attributes
